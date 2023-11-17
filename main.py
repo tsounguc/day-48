3b6b56932c86b63212715857deb22cae3ff08d3b
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-11-17 13:02:56 -0500

    installing and setting up Selenium

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+from selenium import webdriver
+# Keep browser open after program finishes by getting a hold of Chrome options
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://www.amazon.com")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Close browser programmatically
+# close() closes the particular tab
+# driver.close()
+# quit() closes the entire browser/program
+# driver.quit()
