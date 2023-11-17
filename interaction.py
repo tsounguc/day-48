01e9cf80a0c6e0b26ea2662d0b2a8749e8a8ea77
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-11-17 17:25:27 -0500

    How to prgramatically enter info in text field

--- interaction.py
+++ interaction.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 # Instantiate options
 chrome_options = webdriver.ChromeOptions()
@@ -14,8 +15,21 @@ driver.get("https://en.wikipedia.org/wiki/Main_Page")
 article_count = driver.find_element(By.CSS_SELECTOR,'#articlecount a')
 print(article_count.text)
 
-article_count.click()
+# article_count.click()
+
+view_history = driver.find_element(By.LINK_TEXT, "View history")
+view_history.click()
+
+search_button = driver.find_element(By.CSS_SELECTOR, "#p-search a")
+search_button.click()
+
+search_bar = driver.find_element(By.NAME, "search")
+search_bar.send_keys("Python")
+
+search_bar = driver.find_element(By.NAME, "search")
+search_bar.send_keys(Keys.ENTER)
+
 
 
 # close website
-driver.quit()
+# driver.quit()
